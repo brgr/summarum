37b3ae8d835f042a893cd876893e4b974ea23bc6
Dominik Berger <dominik.berger@protonmail.com>
2024-04-11 23:30:24 +0200

    Improve parsing slightly

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -1,16 +1,34 @@
+
+// TODO: I need to begin parsing into well-defined types
+type VariableAssignment = [string, string];
+
+
+function parseLine(line: string): VariableAssignment | undefined {
+    let lineResult: VariableAssignment | undefined = undefined;
+
+    if (line.includes('=')) {
+        const [variable, value] = line.split('=');
+        lineResult = [variable, value];
+    }
+
+    return lineResult;
+}
+
 export function parseTextArea(textAreaContent: string): Record<string, string> {
-    console.log("called");
     const results: Record<string, string> = {};
 
     const cleanTextAreaContent = textAreaContent
         .replace(/<div>/g, '')
-        .replace(/<\/div>/g, '');
+        .replace(/<\/div>/g, '')
+        .replace(/&nbsp;/g, ' ')
+        .replaceAll(/ +/g, ' ');
 
     const lines = cleanTextAreaContent.split('<br>');
 
     lines.forEach(line => {
-        if (line.includes('=')) {
-            const [variable, value] = line.split('=');
+        const lineResult = parseLine(line);
+        if (lineResult) {
+            const [variable, value] = lineResult;
             results[variable.trim()] = value.trim();
         }
     });
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -5,3 +5,13 @@ test('test parser', () => {
     const results = parseTextArea("x = 2<br><br><div>y = x + 1</div><div><br></div><div>z = y + z<br></div>")
     expect(results).toEqual({x: '2', y: 'x + 1', z: 'y + z'})
 })
+
+test('test parser with too much whitespace', () => {
+    const results = parseTextArea("x = 2<br><br><div>  y = x + 1</div><div><br></div><div>z = y + z<br></div>")
+    expect(results).toEqual({x: '2', y: 'x + 1', z: 'y + z'})
+})
+
+test('test parser with too much whitespace 2' , () => {
+    const results = parseTextArea("x = 2<br><br><div>y = x + 1</div><div><br></div><div>z = x + x&nbsp;&nbsp;&nbsp;&nbsp; + x<br></div>")
+    expect(results).toEqual({x: '2', y: 'x + 1', z: 'x + x + x'})
+})
