1bae43184681dbd14d9aeec003ba37d7fbca854e
Dominik Berger <dmnkbrgr@gmail.com>
2024-09-27 22:53:13 +0200

    Add first working Playwright test in CalculatorTextarea.spec.ts
    0

--- tests-playwright/CalculatorTextarea.spec.ts
+++ tests-playwright/CalculatorTextarea.spec.ts
@@ -1,54 +1,21 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-test('go to my page', async ({ page }) => {
-    await page.goto('/');
+test('CalculatorTextarea: enter a letter in an editor-line', async ({page}) => {
+    // Add --debug to Playwright to be able to debug the browser
+    await page.goto('http://localhost:5173/');
+    let textbox = page.getByRole('textbox');
+    await textbox.locator('div').click();
+    await textbox.fill('1000');
 
-    let textarea = page.getByLabel('calculator-textarea');
-
-    let innerHTML = await textarea.innerHTML()
-
-    // Expect a title "to contain" a substring.
-    console.log(textarea);
+    let innerHTML = await textbox.innerHTML();
     console.log(innerHTML);
-    // TODO: This works now. Now actually write the test(s)
-});
 
+    let textContent = await textbox.textContent();
+    console.log(textContent);
 
+    // First is the input, second is the (non-editable) math output
+    expect(textContent).toEqual('1000 1000');
 
-// test('CalculatorTextarea renders the textarea', () => {
-//     const {container} = render(CalculatorTextarea);
-//
-//     const textarea = container.querySelector('.calculator-textarea');
-//
-//     expect(textarea).not.toBeNull();
-//     expect(textarea).toBeInTheDocument();
-//     // @ts-ignore
-//     expect(textarea.textContent).toBe('');
-// });
-
-test('CalculatorTextarea: enter a letter in an editor-line', async ({ page }) => {
-    // write the above test now in playwright
-    await page.goto('/');
-
-    let textarea = page.getByLabel('calculator-textarea');
-
-    // Wait until textarea has a child div with class editor-line
-    await expect(textarea.locator('div.editor-line')).toBeVisible();
-
-
-    let innerHTML = await textarea.innerHTML();
-    console.log('innerHTML', innerHTML);
-
-    await textarea.fill('a');
-
-    // remove focus
-
-    innerHTML = await textarea.innerHTML();
-    console.log('innerHTML', innerHTML);
-    let divInInnerHTML = textarea.locator('div').first();
-    // check the class
-    await expect(divInInnerHTML).toHaveClass(/editor-line/); // FIXME: It seems like this really isn't working for Firefox... I need to debug deeper inside
-    await expect(divInInnerHTML).toHaveAttribute('contenteditable', 'true');
-    expect(await divInInnerHTML.innerHTML()).toEqual('a');
-    expect(innerHTML).toContain('a');
+    // just wait forever
+    // await new Promise(() => {});
 });
